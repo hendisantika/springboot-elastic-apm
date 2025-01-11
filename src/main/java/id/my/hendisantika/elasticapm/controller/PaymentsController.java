d969babdc5f0842753b7fe7472221ff985197bc2
hendisantika <hendisantika@yahoo.co.id>
2025-01-12 06:36:40 +0700

    Add PaymentsController🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/elasticapm/controller/PaymentsController.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.elasticapm.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-elastic-apm
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/01/25
+ * Time: 06.36
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@RestController
+public class PaymentsController {
+}
