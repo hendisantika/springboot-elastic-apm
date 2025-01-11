745f66067df080c77956c9f066501eed0f519d1d
hendisantika <hendisantika@yahoo.co.id>
2025-01-12 06:34:44 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/elasticapm/SpringbootElasticApmApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.elasticapm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootElasticApmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootElasticApmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/elasticapm/SpringbootElasticApmApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.elasticapm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootElasticApmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
